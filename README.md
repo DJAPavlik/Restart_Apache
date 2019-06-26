ab8984f73dc347fb13d0d410f1ae7218e9b8576a
Dennis Pavlik <DJAPavlik@gmail.com>
2019-06-26 14:13:39 -0500

    Added two parameters for vhost and command

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Restart_Apache_New
 A simple bash script for reloading Apache 
+
+
+## Usage
+Clone the repository or download the latest release. 
+
+From a command line call re.sh with two arguments.
+1. The vhost configuration
+2. The service directive {restart|reload}
+
+e.g.
+./re.sh 000* restart
--- re.sh
+++ re.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-# Move the current execution state to the proper directory
-cd /etc/apache2/sites-available
+# Accept two command line parms
+#   parm 1 = virtual host configuration
+#   parm 2 = which service directive we wanted to use
+CONFIG="$1"
+COMMAND="$2"
 
-# Disable a vhost configuration
-sudo a2dissite *
-sudo service apache2 restart
+# Insure two parms were entered
+if [ $# -ne 2 ]
 
-# Enable a vhost configuration
-sudo a2ensite *
-sudo service apache2 restart
+    # Move the current execution state to the proper directory
+    cd /etc/apache2/sites-available
+
+    # Disable a vhost configuration
+    sudo a2dissite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+
+    # Enable a vhost configuration
+    sudo a2ensite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+
+then
+    echo "$0 requires two parameters {virtual-host} {restart|reload}"
+    exit 1
+fi
